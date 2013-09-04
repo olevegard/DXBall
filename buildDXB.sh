d4d2547117c8d3222fada726dbf699be2495f0e4
Ole Vegard <oleveg@gmail.com>
2013-09-04 14:56:19 +0200

    Improved buildDXB.sh
    Added -r to specify that the program should run if the compilation
    eucceeds

--- buildDXB.sh
+++ buildDXB.sh
@@ -1,7 +1,29 @@
-#!/bin/sh
+#!/bin/bash
 
+# Let qmake create a makefile
 qmake -o Makefile build/DXBall.pro
-if [ make ]; then
- 	./DXBall -lPlayer Ole_Vegard -rPlayer Openent
-	run
+
+# Clear console window so it's easier to see make output
+clear
+
+# Try to build the project
+if make; then
+
+	# Build succesfull, no need to see make output
+	clear
+	echo 'Compilation succesfull'
+
+	# Check arguments
+	while getopts ":r" opt; do
+		case $opt in
+			r)
+				echo 'Running program`'
+				./DXBall -lPlayer Ole_Vegard -rPlayer Openent
+				;;
+			\?)
+				echo "Invalid option: -$OPTARG" >&2
+				;;
+		esac
+	done
+	exit 0
 fi
