59e530bd26c129fce92558faf0b0b8c05958b322
Ole Vegard <oleveg@gmail.com>
2013-09-05 12:46:14 +0200

    Improved build script, addded command line options
    	See updated readme for details on the availabel commands

--- buildDXB.sh
+++ buildDXB.sh
@@ -3,27 +3,77 @@
 # Let qmake create a makefile
 qmake -o Makefile build/DXBall.pro
 
-# Clear console window so it's easier to see make output
-clear
+RunGame=false
+RunGDB=false
+RunValgrind=false
+
+RunString="./DXBall -lPlayer Me -rPlayer You -fpsLimit 0 "
+GDBString="gdb -ex run --args $RunString"
+ValgindString="valgrind \
+--suppressions=valgrind/ignore \
+--tool=memcheck \
+--leak-check=full \
+--leak-resolution=high \
+--error-limit=no \
+--track-origins=yes \
+--undef-value-errors=yes \
+--read-var-info=yes \
+$RunString"
 
 # Try to build the project
 if make; then
 
 	# Build succesfull, no need to see make output
 	clear
-	echo 'Compilation succesfull'
+	echo "Compilation succesfull"
 
 	# Check arguments
-	while getopts ":r" opt; do
+	while getopts ":rvg" opt; do
 		case $opt in
 			r)
-				echo 'Running program`'
-				./DXBall -lPlayer Ole_Vegard -rPlayer Openent
+				RunGame=true
+				RunGDB=false
+				RunValgrind=false
+				;;
+			v)
+				RunGame=false
+				RunValgrind=true
+				RunGDB=false
+				;;
+			g)
+				RunGame=false
+				RunGDB=true
+				RunValgrind=false
 				;;
 			\?)
 				echo "Invalid option: -$OPTARG" >&2
 				;;
 		esac
+
 	done
-	exit 0
-fi
+
+	if $RunGame ; then
+		echo -e "\tNormal mode"
+		echo -e "\tCommand : " $RunString
+		echo "=============================== DX Balll ==============================="
+		$RunString
+	fi
+
+	if $RunGDB ; then
+		echo -e "\tDebug mode"
+		echo -e "\tCommand : " $GDBString
+		echo "=============================== DX Balll ==============================="
+		$GDBString
+	fi
+
+	if $RunValgrind ; then
+		echo -e "\tValgrind mode"
+		echo -e "\tCommand : " $ValgindString
+		echo "=============================== DX Balll ==============================="
+		$ValgindString
+	fi
+fi # if make
+exit 0
+#gdb -ex run ./DXBall
+#fi
+#./DXBall -lPlayer Ole_Vegard -rPlayer Openent -fpslimit 0
