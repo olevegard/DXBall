008a06201e13970f10ab4dac28aace56b7aa306f
Ole Vegard <oleveg@gmail.com>
2013-09-19 19:25:37 +0200

    Level loaded from file

--- BoardLoader.h
+++ BoardLoader.h
@@ -1,7 +1,13 @@
+
 #pragma once
 #include <algorithm>
-#include <iostream>
 
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <sstream>
+
+// The location and type of a single tile
 struct TilePosition
 {
 	TilePosition()
@@ -15,18 +21,74 @@ struct TilePosition
 	{
 
 	}
+/*
+	TilePosition& operator=( TilePosition &&pos )
+	{
+		xPos = pos.xPos;
+		yPos = pos.yPos;
+		type = pos.type;
 
+		return *this;
+	}
+	*/
 	short xPos;
 	short yPos;
 
 	TileTypes type;
 };
 
+inline std::istream& operator>>( std::istream &is, TilePosition &pos )
+{
+	int type = 0;
+	is >> pos.xPos >> pos.yPos >> type;
+	pos.type = static_cast< TileTypes > ( type );
+
+	if ( type < 0 || type > 5 )
+	{
+		is.setstate( std::ios_base::failbit );
+		std::cout << "Error! " << type << " is not a valid tyle type!" << std::endl;
+	}
+
+	return is;
+}
+inline std::ostream& operator<<( std::ostream &os, const TilePosition &pos )
+{
+	os << std::setw(4) << pos.xPos << " " << std::setw(4) << pos.yPos << " ";
+
+	switch ( pos.type )
+	{
+		case TileTypes::Regular:
+			os << "Regular";
+			break;
+		case TileTypes::Hard:
+			os << "Hard";
+			break;
+		case TileTypes::Unbreakable:
+			os << "Unbreakable";
+			break;
+		case TileTypes::Explosive:
+			os << "Explosive";
+			break;
+		default:
+			os << "Unknown";
+			break;
+
+	}
+	os << std::endl;
+
+	return os;
+}
+
+// An enire board
 struct Board
 {
 	void AddTile( short xPos, short yPos, TileTypes tt )
 	{
 		tiles.push_back( TilePosition( xPos, yPos, tt  ) );
+	}
+	void AddTile( TilePosition pos )
+	{
+		tiles.push_back( pos );
 	}
 
 	/*
@@ -34,11 +96,22 @@ struct Board
 	{
 		short tileHeight = 25;
 		short boardHeight = ( countRows * tileHeight );
+
 		short totalVerticalFreeSpace = static_cast< short > (windowSize.h - boardHeight  + 20 );
 		short halfVerticalFreeSpace = static_cast< short > ( totalVerticalFreeSpace / 2 );
 	}*/
 
+	void MoveVertically( short amount )
+	{
+		auto moveDown_Func = [amount]( TilePosition tt )
+		{
+			tt.yPos += amount;
+			return tt;
+		};
 
+		std::transform( tiles.begin(), tiles.end(),tiles.begin(),  moveDown_Func );
+
+	}
 	void CountRows( )
 	{
 		std::vector<int> yValues;
@@ -79,6 +152,60 @@ struct BoardLoader
 	}
 	void GenerateBoard( const SDL_Rect &windowSize )
 	{
+		std::ifstream boardFile( "boards/board.txt" );
+		//boardFile.
+		/*	.
+		while ( !boardFile.eof() ) //&& next != '#' )
+		{
+			boardFile >> pos;
+			std::cout << pos;
+			board.AddTile( pos );
+
+			next = static_cast< char > ( boardFile.peek() );
+		}
+		for ( TilePosition pos ; boardFile >> pos ; )
+		{
+			//next = static_cast< char > ( boardFile.peek() );
+			std::cout << "\tLoading tile : " << pos;
+			board.AddTile( pos );
+		}*/
+
+		//board.MoveVertically( 100 );
+		//
+		TilePosition pos;
+		std::string line;
+		//while ( !boardFile.eof( ) && !boardFile.fail() ) //&& next != '#' )
+		while ( getline( boardFile, line ) )
+		{
+			if ( line[0] == '#' || line.empty() )
+			{
+				std::cout << "Skipping : " << line << std::endl;
+				continue;
+			} else
+			{
+				std::cout << "Adding : " << line << std::endl;
+			}
+
+			std::stringstream( line ) >> pos;
+			std::cout << "Adding : " << pos;
+			board.AddTile( pos );
+			std::cout << "===========================================================================\n";
+			/*
+			//next =  static_cast< char > ( boardFile.get(next );
+			boardFile.get(next );
+			if ( next == '#' ) 
+				boardFile.get();
+
+			boardFile >> pos;
+			std::cout << pos;
+			board.AddTile( pos );
+			*/
+		}
+		std::cout << windowSize.h;
+
+/*
+		boardFile >> pos;
+		std::cout << "File : " << pos;
 		short x = 60;
 		short y = 200;
 
@@ -522,6 +649,6 @@ struct BoardLoader
 		board.AddTile( x, y, TileTypes::Regular );
 		x += 65;
 		board.AddTile( x, y, TileTypes::Regular );
-
+*/
 	}
 };
