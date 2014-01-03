d699532b3ede5301e41030692e41dc0cf7179c2e
olevegard <oleveg@gmail.com>
2014-01-03 10:25:32 +0100

    Restructure of enum headers
    -MessageType has been reformated to make it easier to read.
    -GameState has been reformated and descriptions has been added.

--- enums/GameState.h
+++ enums/GameState.h
@@ -2,13 +2,13 @@
 
 enum class GameState
 {
-	MainMenu,
-	Lobby,
-	InGame,
-	Paused,
-	InGameMenu,
-	InGameWait,
-	GameOver,
-	Options,
-	Quit
+	MainMenu,	//0 - Self explanatory
+	Lobby,		//1 - Muliplayer menu
+	InGame, 	//2 - Game has been started and is active
+	Paused, 	//3 - Game is paused
+	InGameMenu, //4 - Not used ?
+	InGameWait, //5 - A mulitplayer game has been started, but player 2 hasn't joined
+	GameOver,	//6 - Game is over, display winner
+	Options,	//7 - Display options ( not implemented )
+	Quit 		//8 - Game is to be quit
 };
--- enums/MessageType.h
+++ enums/MessageType.h
@@ -2,14 +2,14 @@
 
 enum MessageType
 {
-	PaddlePosition, // Contains paddle position
-	PaddleResize, 	// Contains new paddle size ( for use with paddle shrik / enlarge )
+	PaddlePosition, 	// Contains paddle position
+	PaddleResize, 		// Contains new paddle size ( for use with paddle shrik / enlarge )
 
-	BallSpawned, 	// New ball spawned. Contains ID, position, speed and direction.
-	BallData,    	// ID, position, speed, direction
-	BallKilled,  	// ID of ball
+	BallSpawned, 		// New ball spawned. Contains ID, position, speed and direction.
+	BallData,    		// ID, position, speed, direction
+	BallKilled,  		// ID of ball
 
-	TileHit,		// Player hit a tile, remove tile, reduce tile HP or trigger explosion.
+	TileHit,			// Player hit a tile, remove tile, reduce tile HP or trigger explosion.
 
 	GameSettings,		// Contains resolution of the remote player. Sent during beggining and when resolution chages.
 	GameStateChanged,	// Game was paused by remote player
@@ -23,8 +23,9 @@ enum MessageType
 	PlayerName,			// Tell the other player your name
 	LevelDone,			// Tell the other player that the level is done on your side
 
-	BonusSpawned,	// A bonus was triggeredby the oponenyyt
-	BonusPickup, 	// A bonus was picked up by the oponent
+	BonusSpawned,		// A bonus was triggeredby the oponenyyt
+	BonusPickup, 		// A bonus was picked up by the oponent
 
-	BulletFire,
+	BulletFire,			// Bullet was fired from oponent
+	BulletKilled,		// Bullet fired from opnent was killed
 };
