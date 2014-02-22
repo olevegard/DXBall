76de8bc473523b30861bc8449139e0a07a85c59b
olevegard <oleveg@gmail.com>
2014-02-22 10:28:33 +0100

    Added FlipAndScale()
    -Takes a Vector2f, a factor and the height of the object.
    -Scales the Vector2f using Math::Scale() and then subtracts height from
    the Vector2f's y value
    -Returns the Vector2f

--- math/VectorHelpers.h
+++ math/VectorHelpers.h
@@ -6,9 +6,18 @@ namespace Math
 {
 
 	Vector2f Scale( Vector2f vec, double factor );
+	Vector2f FlipAndScale( Vector2f vec, double factor, double height  );
+
 	Vector2f Scale( Vector2f vec, double factor )
 	{
 		return Vector2f( vec.x * factor, vec.y * factor );
 	}
 
+
+	Vector2f FlipAndScale( Vector2f vec, double factor, double height )
+	{
+		Vector2f retVec = Scale( vec, factor );
+		retVec.y -= height;
+		return retVec;
+	}
 }
