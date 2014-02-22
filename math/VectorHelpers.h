aa15f358cfb0ba587f479f5c9d21eb010b8cf511
olevegard <oleveg@gmail.com>
2014-02-22 10:10:04 +0100

    Added Math::Scale()
    -Takes a Vector2f and a scale factor
    -Returns a new Vector2f with x and y values of the Vector2f mulitpled
    with the factor
    -Does not modify the Vector2f.

--- math/VectorHelpers.h
+++ math/VectorHelpers.h
@@ -2,3 +2,13 @@
 
 #include<ostream>
 
+namespace Math
+{
+
+	Vector2f Scale( Vector2f vec, double factor );
+	Vector2f Scale( Vector2f vec, double factor )
+	{
+		return Vector2f( vec.x * factor, vec.y * factor );
+	}
+
+}
