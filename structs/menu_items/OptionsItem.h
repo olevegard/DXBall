63d69fcbd4b32530a9bce08ac36cc1d5614c165d
olevegard <oleveg@gmail.com>
2014-03-17 22:26:59 +0100

    Addded OpionsItem
    -Like MenuItem, but with the possibility of shwoing a value
    -Will be used to set optins like Ball speed
    -Will be set by clicking pluss/minus buttons above and under the value.

---
+++ structs/menu_items/OptionsItem.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <SDL2/SDL.h>
+#include <string>
+
+#include "MenuItem.h"
+
+struct OptionsItem : public MenuItem
+{
+	OptionsItem( std::string name)
+		:	MenuItem( name )
+	{
+	}
+
+	OptionsItem() = delete;
+
+	void SetValueTexture( SDL_Texture* texture )
+	{
+		valueTexture = texture;
+	}
+	void SetValueRect( SDL_Rect r )
+	{
+		valueRect = r;
+	}
+	SDL_Texture* GetValueTexture( ) const
+	{
+		return valueTexture;
+	}
+	SDL_Rect GetValueRect( ) const
+	{
+		return valueRect;
+	}
+	const SDL_Rect* GetValueRectP( ) const
+	{
+		return &valueRect;
+	}
+	private:
+		SDL_Texture* valueTexture;
+		SDL_Rect valueRect;
+		//int32_t optionsValue;
+};
+
